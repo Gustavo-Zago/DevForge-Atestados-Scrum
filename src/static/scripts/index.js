bfdd5362a4329499298fda7ffffe33e327f4eb1e
Gustavo-Zago <rrgusta@gmail.com>
2025-03-21 19:34:26 -0300

    Criação da estilização de buscar atestado, modificação no HTML de Espera.HTML arquivo index.js

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -5,55 +5,68 @@ gestaoButton = document.getElementById("btn_gestao");
 equipeButton = document.getElementById("btn_equipe");
 admButton = document.getElementById("btn_adm");
 closeButton = document.getElementById("close_modal");
-
 adm_btn_senha = document.querySelector("#adm_btn_senha");
-
+buscarButton = document.getElementById("btn_buscar");
 const adm_password = "admin";
 
-atestadoButton.addEventListener("click", function () {
-  redirecionar("/envio");
-});
-
-equipeButton.addEventListener("click", function () {
-  redirecionar("/scrum");
-});
-
-gestaoButton.addEventListener("click", function () {
-  modalAction("open");
-
-  adm_btn_senha.addEventListener("click", function (e) {
-    e.preventDefault();
-    //alert("1")
-    let input_senha = document.querySelector("#inp_senha").value;
-    alert(input_senha)
-    if (input_senha == adm_password){
-      alert("Hello World!");
-      redirecionar("/adminatestado");
-      return
-    }
+// Verifica se o botão "atestadoButton" existe no DOM
+if (atestadoButton) {
+  atestadoButton.addEventListener("click", function () {
+    redirecionar("/envio");
   });
+}
 
-});
-
-admButton.addEventListener("click", function () {
-  modalAction("open");
-
-  adm_btn_senha.addEventListener("click", function (e) {
-    e.preventDefault();
-    //alert("1")
-    let input_senha = document.querySelector("#inp_senha").value;
-    alert(input_senha)
-    if (input_senha == adm_password){
-      alert("Hello World!");
-      redirecionar("/adminscrum");
-      return
-    }
+if (equipeButton) {
+  equipeButton.addEventListener("click", function () {
+    redirecionar("/scrum");
   });
-});
+}
 
-closeButton.addEventListener("click", () => {
-  modalAction("close");
-});
+if (gestaoButton) {
+  gestaoButton.addEventListener("click", function () {
+    modalAction("open");
+
+    adm_btn_senha.addEventListener("click", function (e) {
+      e.preventDefault();
+      let input_senha = document.querySelector("#inp_senha").value;
+      alert(input_senha);
+      if (input_senha == adm_password) {
+        alert("Hello World!");
+        redirecionar("/adminatestado");
+        return;
+      }
+    });
+  });
+}
+
+if (admButton) {
+  admButton.addEventListener("click", function () {
+    modalAction("open");
+
+    adm_btn_senha.addEventListener("click", function (e) {
+      e.preventDefault();
+      let input_senha = document.querySelector("#inp_senha").value;
+      alert(input_senha);
+      if (input_senha == adm_password) {
+        alert("Hello World!");
+        redirecionar("/adminscrum");
+        return;
+      }
+    });
+  });
+}
+if(buscarButton)
+{
+  buscarButton.addEventListener("click", function () {
+    modalAction("open")
+  });
+}
+
+if (closeButton) {
+  closeButton.addEventListener("click", () => {
+    modalAction("close");
+  });
+}
 
 function redirecionar(url) {
   window.location.href = url;
