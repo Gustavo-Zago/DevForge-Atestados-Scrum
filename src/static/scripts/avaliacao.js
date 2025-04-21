e677475073ca9c0aa2d77e66b84abdcf41b987c9
Lucas Pereira <lucaspereira.dev@proton.me>
2025-04-20 22:50:06 -0300

    feat(#18): Perguntas dinâmicas e Sistema de avaliacao

--- src/static/scripts/avaliacao.js
+++ src/static/scripts/avaliacao.js
@@ -1,12 +1,176 @@
-listaBotoes = document.querySelectorAll(".btnAvalia");
+const buttonsAvalia = document.querySelectorAll(".btnAvalia");
+const buttonsPergunta = document.querySelectorAll(".Fimdapagina");
+const selectEquipe = document.getElementById("teamselection");
+const selectAvaliador = document.getElementById("Avaliador");
+const selectAvaliado = document.getElementById("Avaliado");
 
-listaBotoes.forEach((botao) => {
-  botao.addEventListener("click", () => {
-    const jaSelecionado = botao.classList.contains("selecionado");
-    listaBotoes.forEach((b) => b.classList.remove("selecionado"));
+const btnProx = document.getElementById("proximo");
+const btnAnt = document.getElementById("anterior");
+let selectedNotas = {};
+let atualNota = NaN;
+let currentIndex = 0;
 
-    if (!jaSelecionado) {
-      botao.classList.add("selecionado");
-    }
+inicializaButtonClick();
+
+function inicializaButtonClick() {
+  disableButtonClick();
+  disableProximoCarrosel();
+  atualizarBotao();
+
+  buttonsAvalia.forEach((button) => {
+    button.addEventListener("click", () => handleButtonClick(button));
   });
+}
+
+function handleButtonClick(button) {
+  const jaSelecionado = button.classList.contains("selecionado");
+
+  removeButtonValue();
+
+  if (!jaSelecionado) {
+    button.classList.add("selecionado");
+    enableProximoCarrosel();
+    statusSelect(true);
+    atualNota = button.getAttribute("data-nota");
+    console.log(atualNota);
+  } else {
+    disableProximoCarrosel();
+  }
+}
+
+function removeButtonValue() {
+  buttonsAvalia.forEach((b) => b.classList.remove("selecionado"));
+}
+
+function removeSelectValue() {
+  selectEquipe.value = "None";
+  selectAvaliador.value = "None";
+  selectAvaliado.value = "None";
+}
+
+selectEquipe.addEventListener("change", verificaValor);
+
+selectAvaliador.addEventListener("change", () => {
+  verificaValor();
+  Array.from(selectAvaliado).forEach((opt) => (opt.disabled = false));
+  selectAvaliado.querySelector(
+    `option[value="${selectAvaliador.value}"]`
+  ).disabled = true;
+});
+
+selectAvaliado.addEventListener("change", verificaValor);
+
+function verificaValor() {
+  const allSelect =
+    selectEquipe.value !== "None" &&
+    selectAvaliador.value !== "None" &&
+    selectAvaliado.value !== "None";
+
+  allSelect ? enableButtonClick() : disableButtonClick();
+  console.log(allSelect);
+}
+
+function statusSelect(status) {
+  selectEquipe.disabled = status;
+  selectAvaliador.disabled = status;
+  selectAvaliado.disabled = status;
+}
+
+function enableButtonClick() {
+  buttonsAvalia.forEach((b) => b.classList.remove("noClick"));
+}
+
+function disableButtonClick() {
+  buttonsAvalia.forEach((b) => {
+    b.classList.add("noClick");
+    b.classList.remove("selecionado");
+  });
+}
+
+function enableProximoCarrosel() {
+  btnProx.disabled = false;
+}
+
+function disableProximoCarrosel() {
+  btnProx.disabled = true;
+}
+
+function setNotas() {
+  localStorage.setItem(
+    selectEquipe.value +
+      "-" +
+      selectAvaliador.value +
+      "-" +
+      selectAvaliado.value,
+    JSON.stringify(selectedNotas)
+  );
+}
+
+function removeUltimaChave() {
+  const chaves = Object.keys(selectedNotas);
+  const ultimaChave = chaves.at(-1);
+  delete selectedNotas[ultimaChave];
+  setNotas();
+}
+
+//Carrosel
+const wrapper = document.querySelector(".carrosel-wrapper");
+const spanAvaliacao = document.getElementById("tipoAvalicao");
+const tipoAvaliacao = [
+  "Proatividade",
+  "Autonomia",
+  "Cooperação",
+  "Entrega de Resultados",
+];
+
+function exibirPergunta(index) {
+  const offset = index * -100;
+  wrapper.style.transform = `translateX(${offset}%)`;
+  spanAvaliacao.textContent = tipoAvaliacao[index];
+  atualizarBotao();
+}
+
+function atualizarBotao() {
+  btnAnt.disabled = currentIndex === 0;
+
+  function resetAvaliacao() {
+    currentIndex = 0;
+    removeButtonValue();
+    removeSelectValue();
+    disableButtonClick();
+    disableProximoCarrosel();
+    atualizarBotao();
+    statusSelect(false);
+    exibirPergunta(currentIndex);
+  }
+
+  if (currentIndex === 3) {
+    btnProx.textContent = "Enviar";
+    btnProx.addEventListener("click", () => {
+      resetAvaliacao();
+    });
+  } else {
+    btnProx.textContent = "Próximo";
+  }
+}
+
+btnProx.addEventListener("click", () => {
+  selectedNotas[tipoAvaliacao[currentIndex]] = atualNota;
+  setNotas();
+  removeButtonValue();
+
+  currentIndex + 1 < tipoAvaliacao.length ? currentIndex++ : currentIndex;
+  exibirPergunta(currentIndex);
+
+  console.log(selectedNotas);
+});
+
+btnAnt.addEventListener("click", () => {
+  removeUltimaChave();
+  removeButtonValue();
+
+  currentIndex > 0 && currentIndex--;
+  exibirPergunta(currentIndex);
+
+  console.log(selectedNotas);
 });
