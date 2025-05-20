324d2d85f68791e9ad1bb224336e74ab7d9956f9
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2025-05-20 16:50:20 -0300

    fix: Cria arquivo de notas

--- src/app.py
+++ src/app.py
@@ -142,24 +142,29 @@ def enviarNotas():
     avaliador = dados.get("Avaliador")
     avaliado = dados.get("Avaliado")
     notas = dados.get("notas")
-    try:
-        with open(UPLOAD_EQUIPE+equipeNome+".txt", "r", encoding='utf-8') as fileR:
-            arquivo = fileR.readlines()
-            arquivo_reescrito = []
+    pathArquivo = UPLOAD_EQUIPE+equipeNome+".txt"
+    if os.path.exists(pathArquivo):
+        try:
+            with open(pathArquivo, "r", encoding='utf-8') as fileR:
+                arquivo = fileR.readlines()
+                arquivo_reescrito = []
 
-            for linha in arquivo:
-                if linha.startswith(f"{avaliador} - {avaliado}"):
-                    nova_linha = f"{avaliador} - {avaliado}: {notas}\n"
-                    arquivo_reescrito.append(nova_linha)
-                else:
-                    arquivo_reescrito.append(linha)
+                for linha in arquivo:
+                    if linha.startswith(f"{avaliador} - {avaliado}"):
+                        nova_linha = f"{avaliador} - {avaliado}: {notas}\n"
+                        arquivo_reescrito.append(nova_linha)
+                    else:
+                        arquivo_reescrito.append(linha)
 
-        with open(UPLOAD_EQUIPE+equipeNome+".txt", "w", encoding='utf-8') as fileW:
-            fileW.writelines(arquivo_reescrito)
+            with open(pathArquivo, "w", encoding='utf-8') as fileW:
+                fileW.writelines(arquivo_reescrito)
 
-        return jsonify({"mensagem": "Nota adicionada com sucesso!"}), 200
-    except Exception as e:
-        return jsonify({"Erro": str(e)}), 500
+            return jsonify({"mensagem": "Nota adicionada com sucesso!"}), 200
+        except Exception as e:
+            return jsonify({"Erro": str(e)}), 500
+    with open(pathArquivo, "a", encoding='utf-8') as fileA:
+        fileA.writelines(f"{avaliador} - {avaliado}: {notas}\n")
+    return jsonify({"mensagem": "Nota adicionada com sucesso!"}), 200
 
 @app.route('/adminatestado')
 def adminatestado():
